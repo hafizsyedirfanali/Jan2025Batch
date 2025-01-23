256fc9736ed525755155acb7d23fa0e9f354da50
mohammadfaizan9199 <mohammadfaizan9199@gmail.com>
2025-01-23 19:49:57 +0530

    update

---
+++ 2_ObjectOrientedProgramming/1_Classes.cs
@@ -0,0 +1,42 @@
+﻿namespace _2_ObjectOrientedProgramming;
+/// <summary>
+/// A class is a blueprint.
+/// a class is a collection of class members
+/// such as 
+/// 1. Properties
+/// 2. Fields
+/// 3. Methods
+/// 4. Constructors
+/// 5. Destructor
+/// 6. Classes
+/// 7. Structures (structs)
+/// </summary>
+public class _1_Classes
+{
+    public static void Test()
+    {
+        //A class as a blueprint
+        Student s1 = new Student();
+        s1.age = 10;
+        s1.name = "Abcd";
+        s1.gender = "M";
+        s1.rollNo = 1;
+        s1.mobileNo = "3324324";
+
+        Student s2 = new Student();
+        s2.age = 10;
+        s2.name = "Abcd";
+        s2.gender = "M";
+        s2.rollNo = 1;
+        s2.mobileNo = "3324324";
+
+    }
+    public class Student
+    {
+        public int rollNo;
+        public string name;
+        public int age;
+        public string gender;
+        public string mobileNo;
+    }
+}
---
+++ 2_ObjectOrientedProgramming/2_Properties.cs
@@ -0,0 +1,32 @@
+﻿namespace _2_ObjectOrientedProgramming;
+/// <summary>
+/// A property is a type of variable 
+/// that can be customised for getting and setting
+/// get - is used to get or read the value of property
+/// set - is used to set or add/update/write the value into the property
+/// the methods responsible for getting is called getter and similarly
+/// the methods responsible for setting is called setter
+/// A property is a member of a class
+/// A property is not accessible directly. 
+/// we can access it using getter and setter methods only.
+/// </summary>
+public class _2_Properties
+{
+    public static void Test()
+    {
+        //read operation
+        int i = 10;
+        int j = i;//read operation. reading value of i
+        i = j; //writing operation. writing value into i
+
+        TestClass t = new TestClass();
+        t.ASimpleProperty = 10;//this assigning task is done by setter method
+
+        int value = t.ASimpleProperty;//getter method will copy the value from property and returns that value
+    }
+    public class TestClass
+    {
+        //simple property
+        public int ASimpleProperty { get; set; }
+    }
+}
