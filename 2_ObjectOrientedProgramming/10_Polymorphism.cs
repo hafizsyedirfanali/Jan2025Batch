89c91c6b54eb3e9ddd166e3a8e396e2c72d5dc4a
nikhats252 <nikhats252@gmail.com>
2025-02-07 19:56:20 +0530

    update

---
+++ 2_ObjectOrientedProgramming/10_Polymorphism.cs
@@ -0,0 +1,79 @@
+﻿namespace _2_ObjectOrientedProgramming;
+/// <summary>
+/// Polymorphism is a Greek word that means "many-shaped" and it has two types:
+/// 1. Compile Time Polymorphism - Method Overloading
+/// 2. Run Time Polymorphism - Method Overriding
+/// </summary>
+public class _10_Polymorphism
+{
+    public void Test()
+    {
+
+    }
+    //Compile Time Polymorphism
+    public class CompileTimePolymorphism
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Add(int a, int b, int c)
+        {
+            Console.WriteLine(a + b + c);
+        }
+    }
+    //Run Time Polymorphism
+    public class RunTimePolymorphism
+    {
+        //Parent class when marks a method as virtual then it means that 
+        //parent class is allowing the child class to override this method.
+        public virtual void Display()
+        {
+            Console.WriteLine("Parent Class");
+        }
+    }
+    public class ChildClass : RunTimePolymorphism
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Child Class");
+        }
+    }
+    //Hiding an inherited member using the new keyword
+    public class HidingInheritedMember
+    {
+        public int Age { get; set; }
+        public void Display()
+        {
+            Console.WriteLine("Parent Class");
+        }
+    }
+    public class HidingChildClass : HidingInheritedMember
+    {
+        public new int Age { get; set; }
+        public new void Display()
+        {
+            Console.WriteLine("Child Class");
+        }
+    }
+
+    public void TestMultilevelPolyMorphism()
+    {
+        MultilevelBaseClass multilevelBaseClass = new MultilevelBaseClass();
+        MultilevelBaseClass multilevelDerivedClass1 = new MultilevelDerivedClass1();
+        MultilevelBaseClass multilevelDerivedClass2 = new MultilevelDerivedClass2();
+    }
+    //Multilevel Inheritance uses polymorphism
+    public class MultilevelBaseClass
+    {
+        //Grand Parent class
+    }
+    public class MultilevelDerivedClass1 : MultilevelBaseClass
+    {
+        //Parent class
+    }
+    public class MultilevelDerivedClass2 : MultilevelDerivedClass1
+    {
+        //Child class
+    }
+}
--- 2_ObjectOrientedProgramming/9_Inheritance.cs
+++ 2_ObjectOrientedProgramming/9_Inheritance.cs
@@ -3,6 +3,14 @@
 /// Inheritance is a mechanism in which 
 /// one class acquires the properties and 
 /// behavior of another class.
+/// Advantages of Inheritance:
+/// 1. Code Reusability - We can reuse the fields and methods of the existing class.
+/// 2. Method Overriding
+/// 3. Code Maintenance easy - If we want to change the implementation of a method, we can change in the parent class and it will be reflected in all the child classes.
+/// 4. Polymorphism
+/// 
+/// Types of Inheritance:
+/// 1. Single Inheritance - A class is derived from only one base class.
 /// </summary>
 public class _9_Inheritance
 {
@@ -26,4 +34,74 @@ public class _9_Inheritance
     public class ChildClass : ParentClass
     {
     }
+    //Single Inheritance
+    public class SingleBaseClass//Parent class/Super class
+    {
+
+    }
+    public class SingleDerivedClass : SingleBaseClass
+    {
+        //Child class/Sub class
+    }
+
+    //Multilevel Inheritance
+    public class MultilevelBaseClass
+    {
+        //Grand Parent class
+    }
+    public class MultilevelDerivedClass1 : MultilevelBaseClass
+    {
+        //Parent class
+    }
+    public class MultilevelDerivedClass2 : MultilevelDerivedClass1
+    {
+        //Child class
+    }
+    //Hierarchical Inheritance
+    public class HierarchicalBaseClass
+    {
+        //Parent class
+    }
+    public class HierarchicalDerivedClass1 : HierarchicalBaseClass
+    {
+        //Child class
+    }
+    public class HierarchicalDerivedClass2 : HierarchicalBaseClass
+    {
+        //Child class
+    }
+    public class HierarchicalDerivedClass3 : HierarchicalBaseClass
+    {
+        //Child class
+    }
+    //Hybrid Inheritance: Combination of Hierarchical and Multilevel Inheritance
+    public class HybridBaseClass
+    {
+        //Parent class
+    }
+    public class HybridDerivedClass1 : HybridBaseClass
+    {
+        //Child class
+    }
+    public class HybridDerivedClass2 : HybridBaseClass
+    {
+        //Child class
+    }
+    public class HybridDerivedClass3 : HybridDerivedClass1
+    {
+        //Child class
+    }
+    //Multiple Inheritance: C# does not support multiple inheritance
+    public class MultipleBaseClass1
+    {
+        //Parent class
+    }
+    public class MultipleBaseClass2
+    {
+        //Parent class
+    }
+    //public class MultipleDerivedClass : MultipleBaseClass1, MultipleBaseClass2
+    //{         //Child class
+    //}
+
 }
