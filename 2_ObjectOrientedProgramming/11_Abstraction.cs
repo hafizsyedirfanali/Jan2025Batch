d5b63c231e4404e31d0202f40dd6591a8f986ded
nikhats252 <nikhats252@gmail.com>
2025-02-12 20:08:19 +0530

    update

--- 2_ObjectOrientedProgramming/11_Abstraction.cs
+++ 2_ObjectOrientedProgramming/11_Abstraction.cs
@@ -5,11 +5,12 @@
 /// In other words, it is hiding the implementation and showing the definition.
 /// we can achieve this with abstract classes and interfaces.
 /// </summary>
-public class _10_Abstraction
+public class _11_Abstraction
 {
     public void Test()
     {
-
+        Shape s = new Circle();//Shape class is not capable of performing actions 
+        //therefore we call Circle class because it implements the Shape class
     }
     ///Using Abstract Class : It shows only defination of the method and hides the implementation.
     ///
@@ -18,10 +19,12 @@ public class _10_Abstraction
         public abstract int MyProperty { get; set; }
         public abstract void Draw();//Abstract method: It has no implementation
     }
+   
     public class Circle : Shape //Contract: Circle class must implement all abstract members
     {
         private int _myVar;
-        public override int MyProperty { get
+        public override int MyProperty { 
+            get
             {
                 return _myVar;
             }
@@ -31,6 +34,31 @@ public class _10_Abstraction
             }
         }
         public override void Draw()
+        {
+            Console.WriteLine("Draw Circle");
+        }
+    }
+    //Using Interface - It shows only defination of the method and hides the implementation.
+    public interface IShape
+    {
+        public int MyProperty { get; set; }
+        public void Draw();
+    }
+    public class Triangle : IShape //Its a contract that Triange class will implement IShape interface
+    {
+        private int _myVar;
+        public int MyProperty
+        {
+            get
+            {
+                return _myVar;
+            }
+            set
+            {
+                _myVar = value;
+            }
+        }
+        public void Draw()
         {
             Console.WriteLine("Draw Circle");
         }
---
+++ 2_ObjectOrientedProgramming/12_Encapsulation.cs
@@ -0,0 +1,20 @@
+﻿namespace _2_ObjectOrientedProgramming;
+/// <summary>
+/// Encapsulation is the mechanism that binds together the code and the data it manipulates, 
+/// and keeps both safe from outside interference and misuse.
+/// In simple words, encapsulation is defined as the wrapping up of data under a single unit.
+/// We use access modifiers to achieve encapsulation.
+/// public: Code in any assembly can access this type or member. The accessibility level of the containing type controls the accessibility level of public members of the type.
+///private: Only code declared in the same class or struct can access this member.
+///protected: Only code in the same class or in a derived class can access this type or member.
+///internal: Only code in the same assembly can access this type or member.
+///protected internal: Only code in the same assembly or in a derived class in another assembly can access this type or member.
+///private protected: Only code in the same assembly and in the same class or a derived class can access the type or member.
+/// </summary>
+public class _12_Encapsulation
+{
+    public void Test()
+    {
+
+    }
+}
