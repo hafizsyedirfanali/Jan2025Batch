cc4f1b215a7a400c711aa5a4207b738db8726082
nikhats252 <nikhats252@gmail.com>
2025-02-18 20:05:25 +0530

    update

--- MVCDesignPattern/Controllers/HomeController.cs
+++ MVCDesignPattern/Controllers/HomeController.cs
@@ -2,35 +2,30 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MVCDesignPattern.Models;
 
-namespace MVCDesignPattern.Controllers
+namespace MVCDesignPattern.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    [HttpGet]
+    public IActionResult Index()//function/method called as action
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()//function/method called as action
-        {
-            return View();//default view is index view
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();//default view is privacy view
-        }
-        public IActionResult Irfan()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        return View();//default view is index view
     }
+    [HttpGet]
+    public IActionResult Privacy()
+    {
+        return View();//default view is privacy view
+    }
+    [HttpGet]
+    public IActionResult Irfan()
+    {
+        throw new Exception("test error");
+        return View();
+    }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
 }
