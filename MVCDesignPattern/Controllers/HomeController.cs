c2bbf246a732b60eaec824f3b7cb65e4c921b5ae
nikhats252 <nikhats252@gmail.com>
2025-02-21 19:56:49 +0530

    updaet

--- MVCDesignPattern/Controllers/HomeController.cs
+++ MVCDesignPattern/Controllers/HomeController.cs
@@ -9,16 +9,24 @@ public class HomeController : Controller
     [HttpGet]
     public IActionResult Index()//function/method called as action
     {
+        ViewBag.Message = "Welcome to MVC Design Pattern";
+        ViewBag.Text = "This is a space where you will learn .net";
+        ViewData["Date"] = "20-10-2024";
         return View();//default view is index view
     }
     [HttpGet]
     public IActionResult Privacy()
     {
+        ViewBag.Message = "Welcome to Privacy view in MVC Design Pattern";
+        ViewBag.Text = "This is a space where you will learn .net";
+        ViewBag.Date = DateTime.Now;
         return View();//default view is privacy view
     }
     [HttpGet]
     public IActionResult About()
     {
+        TempData["Course"] = "We are learning .net";
+        TempData["Date"] = DateTime.Now;
         return View();//default view is privacy view
     }
     [HttpGet]
