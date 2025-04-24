bdbf178ecf018d8a53d730dcb9bea2b8f3d73b96
shadab0075 <sq9253377@gmail.com>
2025-04-24 20:01:00 +0530

    update

--- CRUD/Controllers/CourseController.cs
+++ CRUD/Controllers/CourseController.cs
@@ -2,6 +2,7 @@
 using CRUD.Data.DTOs;
 using CRUD.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUD.Controllers;
 /// <summary>
@@ -23,7 +24,8 @@ public class CourseController : Controller
     {
         //Get all the courses from the database
         //and pass it to the view
-        var courses = _dbContext.Courses.ToList();
+        var courses = _dbContext.Courses.ToList();//READ
+        //above line will get translated by Entity Framework Core to SQL query  "select * from Courses"
         return View(courses);
     }
     [HttpGet]
@@ -33,6 +35,7 @@ public class CourseController : Controller
         return View();
     }
     [HttpPost]
+    [ValidateAntiForgeryToken]
     public IActionResult AddCourse(AddCourseViewModel course)
     {
         //This method will add a new course to the database
@@ -44,10 +47,68 @@ public class CourseController : Controller
                 Duration = course.Duration,
                 Fees = course.Fees,
             };
-            _dbContext.Courses.Add(entity);//we did changes in the database context
+            _dbContext.Courses.Add(entity);//we did changes in the database context //CREATE
             _dbContext.SaveChanges();//SaveChanges method is used to save the changes made in the database
+            //this will be translated as insert query
             return RedirectToAction("Courses");
         }
         return View(course);
+    }
+
+    [HttpGet]
+    public IActionResult EditCourse(int id)
+    {
+        //This method will return the view to edit a course
+        var course = _dbContext.Courses.AsNoTracking().FirstOrDefault(x => x.Id == id);
+        if (course == null)
+        {
+            return View("Error");
+        }
+        var model = new EditCourseViewModel
+        {
+            Id = course.Id,
+            Name = course.Name,
+            Duration = course.Duration,
+            Fees = course.Fees,
+        };
+        return View(model);
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult EditCourse(EditCourseViewModel course)
+    {
+        //This method will update the course in the database
+        if (ModelState.IsValid)
+        {
+            var entity = _dbContext.Courses.FirstOrDefault(x => x.Id == course.Id);
+            if (entity == null)
+            {
+                return View("Error");
+            }
+            entity.Name = course.Name;
+            entity.Duration = course.Duration;
+            entity.Fees = course.Fees;
+            _dbContext.SaveChanges();//UPDATE
+            //above line will get translated by Entity Framework Core to SQL query  "update Courses set Name = course.Name, Duration = course.Duration, Fees = course.Fees where Id = course.Id"
+            //Advantage of Entity framework.
+            //if only one record is updated then it will not update the other records.
+            return RedirectToAction("Courses");
+        }
+        return View(course);
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteCourse(int id)
+    {
+        //This method will delete the course from the database
+        var entity = _dbContext.Courses.FirstOrDefault(x => x.Id == id);
+        if (entity == null)
+        {
+            return View("Error");
+        }
+        _dbContext.Courses.Remove(entity);//DELETE
+        _dbContext.SaveChanges();
+        //this will be translated as delete query, delete from Courses where Id = id
+        return RedirectToAction("Courses");
     }
 }
