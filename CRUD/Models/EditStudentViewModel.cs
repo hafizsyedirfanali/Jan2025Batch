d5c8bbffa6de69b35e2543d555fa5000e345d36c
shadab0075 <sq9253377@gmail.com>
2025-04-26 20:06:23 +0530

    update

---
+++ CRUD/Models/EditStudentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CRUD.Data.DTOs;
+
+namespace CRUD.Models;
+
+public class EditStudentViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    [Display(Name = "Contact Number")]
+    public string ContactNo { get; set; }
+    [EmailAddress]
+    public string? Email { get; set; }
+    public string? Address { get; set; }
+    [Range(18, 50, ErrorMessage = "Age must be between 18 and 50.")]
+    public byte Age { get; set; }
+}
