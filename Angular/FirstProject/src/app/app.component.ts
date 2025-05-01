698371ebdedfd67c070df796d61e68a6aa0dec59
Deepak Gayakwad <deepakgayakwad421@gmail.com>
2025-05-01 20:05:09 +0530

    commit from vs code

--- Angular/FirstProject/src/app/app.component.ts
+++ Angular/FirstProject/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from './components/first/first.component';
+import { SecondComponent } from "./components/second/second.component";
+import { FourthComponent } from "./components/fourth/fourth.component";
+import { ThirdComponent } from './components/third/third.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstComponent, SecondComponent,ThirdComponent, FourthComponent],
+  standalone:true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'FirstProject';
 }
