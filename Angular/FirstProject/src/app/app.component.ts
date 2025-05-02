56d3fdff7d9a76a8620cc6572f55582de777c193
Deepak Gayakwad <deepakgayakwad421@gmail.com>
2025-05-02 20:03:39 +0530

    update

--- Angular/FirstProject/src/app/app.component.ts
+++ Angular/FirstProject/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FirstComponent } from './components/first/first.component';
-import { SecondComponent } from "./components/second/second.component";
-import { FourthComponent } from "./components/fourth/fourth.component";
-import { ThirdComponent } from './components/third/third.component';
+import { HeaderComponent } from "./header/header.component";
+import { BodyComponent } from "./body/body.component";
+import { FooterComponent } from "./footer/footer.component";
+import { LayoutComponent } from "./layout/layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FirstComponent, SecondComponent,ThirdComponent, FourthComponent],
+  imports: [HeaderComponent, BodyComponent, FooterComponent, LayoutComponent],
   standalone:true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
