8044aede192a795f9d11a9fc5f1b281ae3a0d241
shadab0075 <sq9253377@gmail.com>
2025-05-05 20:03:16 +0530

    update

--- Angular/FirstProject/src/app/app.component.ts
+++ Angular/FirstProject/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-import { BodyComponent } from "./body/body.component";
 import { FooterComponent } from "./footer/footer.component";
-import { LayoutComponent } from "./layout/layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, BodyComponent, FooterComponent, LayoutComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   standalone:true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- Angular/FirstProject/src/app/app.routes.ts
+++ Angular/FirstProject/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'layout', component: LayoutComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'privacy', component: PrivacyComponent },
+];
